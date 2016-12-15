fb9e0563f5e39d0d5803e4d72bf4e406ad1890a8
Federico Montesino Pouzols <federico.montesino@xfel.eu>
2016-12-15 17:21:54 +0100

    some comments, all flake8 issues fixed

--- compress_hdf5_in2out.py
+++ compress_hdf5_in2out.py
@@ -28,35 +28,31 @@ def add_attributes(item, item_source):
 
 def rewrite_h5_group(item_name, item_val, out_grp):
     if isinstance(item_val, h5py.Dataset):
-        print('Dataset: {0}, with shape: {1}, with compression options: {2}'.
+        print(' dataset: {0}, with shape: {1}, with compression options: {2}'.
               format(item_name, item_val.shape, item_val.compression_opts))
-        # print('dir of dataset:', dir(item_val))
-
-        # enable/disable compression here:
-        # out_grp.create_dataset(item_name, shape=item_val.shape, data=item_val.value)
 
         CHUNK_BLOCK = (10, 1000)
+        # arbitrary minimum dataset size to enable compression
+        MIN_COMPRESS_SIZE = CHUNK_BLOCK[0]*CHUNK_BLOCK[1]
 
-        #if 0 == len(item_val.shape):
-        print('name: ', item_val.name)
+        print('  name: ', item_val.name)
         if item_val.dtype == 'O':
-            # skip
+            # Silently skip some unknown/undecided types in HDF5 file produced
+            # by XTC conversion tools, etc.
             ds = None
-        #elif item_val.name != '/entry_1/image_1/data':
-            # This branch is for 'Scalar datasets don't support chunk/filter options'
-        #    ds = out_grp.create_dataset(item_name, shape=item_val.shape, data=item_val.value)
-        #elif item_val.value.size > CHUNK_BLOCK*CHUNK_BLOCK and\
-        #     min(item_val.value.shape) > CHUNK_BLOCK:
-        elif item_val.value.size > CHUNK_BLOCK[0]*CHUNK_BLOCK[1]:
-            ds = out_grp.create_dataset(item_name, shape=item_val.shape, data=item_val.value, 
-                                        compression='gzip', chunks=CHUNK_BLOCK)
+        elif item_val.value.size >= MIN_COMPRESS_SIZE:
+            # rewrite into output file enabling compression
+            ds = out_grp.create_dataset(item_name, shape=item_val.shape,
+                                        data=item_val.value,
+                                        chunks=CHUNK_BLOCK,
+                                        compression='gzip')
         else:
-            ds = out_grp.create_dataset(item_name, shape=item_val.shape, data=item_val.value)
-        #                                compression='gzip')
-        # set attributes?
+            # not worth compressing
+            ds = out_grp.create_dataset(item_name, shape=item_val.shape,
+                                        data=item_val.value)
+
         if ds:
             add_attributes(ds, item_val)
-        # ds.attrs = item_val.attrs
 
     elif isinstance(item_val, h5py.Group):
         print('Group: ', item_name)
@@ -72,7 +68,6 @@ def rewrite_h5_group(item_name, item_val, out_grp):
         add_attributes(out_grp, item_val)
         for subitem_name, subitem_val in item_val.items():
             rewrite_h5_group(subitem_name, subitem_val, out_grp)
-            
 
     else:
         raise RuntimeError('*** Found an item that is not a group '
@@ -91,12 +86,12 @@ def rewrite_in_to_out(in_fname, out_fname):
             rewrite_h5(in_file, out_file)
 
 if 3 != len(sys.argv):
-    print('Give just input and output file') 
+    print('Give just input and output file')
     sys.exit(-1)
 
 print('Loading from: {0}'.format(sys.argv[1]))
 print('Writing to: {0}'.format(sys.argv[2]))
 
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
     rewrite_in_to_out(sys.argv[1], sys.argv[2])
