7a83bbd32f153affc308e9dedfda7989622db419
Federico Montesino Pouzols <federico.montesino@xfel.eu>
2016-12-19 09:37:47 +0100

    tidy up repetitions loop

--- compression_tests/run_repetitions_compression_tests.sh
+++ compression_tests/run_repetitions_compression_tests.sh
@@ -5,17 +5,28 @@
 # repetitions 160-: `ppc64_cpu --smt=off`
 # Note: repetitions 160-187 were done with a karabo deviceserver holding over 130GB, 
 #  disk->disk tests are very slow because of lack of RAM memory for buffering
+# => repeated again with free memory
 
 DATA_ROOT=~/
 OUTPUT_DIR=~/genwqe_comparison_results_hw_repetitions
 
-i=160;
+i=200;
+
+declare -a datasets=(
+    "id22" 
+    "id30"
+)
+
+echo "* Starting repetitions at: $(date)"
+
 while [[ i -le 1000 ]];
 do
   echo " *****************************************************************************************"
   echo " **************************** Starting repetition ${i} ***********************************"
   echo " *****************************************************************************************"
-  ./compare_compression_fpga.sh ${DATA_ROOT}/cxidb/id22 ${DATA_ROOT}/cxidb/id22/files_to_compare_id22.txt | tee ${OUTPUT_DIR}/compress_comparison_results_id22_rep.${i}.txt
-  ./compare_compression_fpga.sh ${DATA_ROOT}/cxidb/id30 ${DATA_ROOT}/cxidb/id30/files_to_compare_id30.txt | tee ${OUTPUT_DIR}/compress_comparison_results_id30_rep.${i}.txt
+  for dset in "${datasets[@]}"
+  do
+      ./compare_compression_fpga.sh ${DATA_ROOT}/cxidb/${dset} ${DATA_ROOT}/cxidb/${dset}/files_to_compare_${dset}.txt | tee ${OUTPUT_DIR}/compress_comparison_results_${dset}_rep.${i}.txt
+  done
   i=$((i+1));
 done;
