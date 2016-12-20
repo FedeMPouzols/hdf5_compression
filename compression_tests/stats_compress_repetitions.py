33909972acf51dd81c3e7cc3af9aa2ae468f3c48
Federico Montesino Pouzols <federico.montesino@xfel.eu>
2016-12-20 15:18:03 +0100

    flake8 fixes and better summary output

--- compression_tests/stats_compress_repetitions.py
+++ compression_tests/stats_compress_repetitions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """
 Load results for 'many' repetitions and calculate statistics of the
-data rate such as maximum time or minimum rate over all the repetitions, 
+data rate such as maximum time or minimum rate over all the repetitions,
 average, std/variation of the rate, etc.
 """
 from __future__ import print_function
@@ -19,7 +19,7 @@ import numpy as np
 def load_results_file(fname, fields_cnt):
     """
     Expects file_name and fields_cnt float values for every file:
-    file_name, size_before_compress, size_compressed, time_disk_disk, 
+    file_name, size_before_compress, size_compressed, time_disk_disk,
     time_ram_ram, time_ram_null
 
     @param fields_cnt :: how many numeric fields are expected per line/file
@@ -49,27 +49,65 @@ def load_results_file(fname, fields_cnt):
 
 # Define the range of repetitions to grab
 rep_start = 0
-max_reps = 160
+max_reps = 159
 fields_cnt = 5
 
-found_cnt = 7 # this is for id22
+found_cnt = 7   # this is for id22
+fname_pattern = 'compress_comparison_results_id22_rep.{0}.txt'
+
+data_root = '/home/common/fpga_tests_output'
 
 in_data_path = 'genwqe_comparison_results_hw_repetitions'
-fname_pattern = 'compress_comparison_results_id22_rep.{0}.txt'
+
+if data_root:
+    in_data_path = os.path.join(data_root, in_data_path)
 
 results = np.zeros((max_reps, found_cnt, fields_cnt))
 for rep_idx in range(rep_start, max_reps):
     # file names with repetition starting from 1...
     fname = 'compress_comparison_results_id22_rep.{0}.txt'.format(rep_idx+1)
     print('Loading results file: {0}'.format(fname))
-    results[rep_idx, :, :] = load_results_file(os.path.join(in_data_path, fname),
+    results[rep_idx, :, :] = load_results_file(os.path.join(in_data_path,
+                                                            fname),
                                                fields_cnt)
 
-print(results[0, :, :])
+
+def print_indiv_results(results, idx):
+    print('Repetition idx {0}, compression ratio: {0}'.
+          format(results[idx, 0, 0] / results[idx, 0, 1]))
+    print('   Rate, disk I/O: {0}, GB/s'.
+          format(results[idx, 0, 0] / results[idx, 0, 2]/1024/1024))
+    print('   Rate, RAM I/O: {0}, GB/s'.
+          format(results[idx, 0, 0] / results[idx, 0, 3]/1024/1024))
+    print('   Rate, RAM I, null O: {0}, GB/s'.
+          format(results[idx, 0, 0] / results[idx, 0, 4] / 1024 / 1024))
+
 
 # Some checks on minimum/maximum values across all repetitions
-rep_idx = 0
-print('Compression ratio: {0}'.format(results[rep_idx, 0,0]/results[rep_idx, 0,1]))
-print('Rate, disk I/O: {0}, GB/s'.format(results[rep_idx, 0,0]/results[rep_idx, 0,2]/1024/1024))
-print('Rate, RAM I/O: {0}, GB/s'.format(results[rep_idx, 0,0]/results[rep_idx, 0,3]/1024/1024))
-print('Rate, RAM I, null O: {0}, GB/s'.format(results[rep_idx, 0,0]/results[rep_idx, 0,4]/1024/1024))
+print_indiv_results(results, 0)
+
+
+def rate_info_str(rate_data):
+    ''' rate_data assumed in GB/s '''
+    return ('average: {0}, GB/s, stddev: {1}, min: {2}, max: {3}'.
+            format(np.mean(rate_data), np.std(rate_data),
+                   np.amin(rate_data), np.amax(rate_data)))
+
+
+print('*** Overall results ***')
+comp_ratio = results[:, 0, 0] / results[:, 0, 1]
+print('Compression ratio, average: {0}, stddev: {1}'.
+      format(np.mean(comp_ratio), np.std(comp_ratio)))
+
+
+rate_disk_io = results[:, 0, 0] / results[:, 0, 2] / 1024 / 1024
+print('   Rate, disk I/O, {0}'.
+      format(rate_info_str(rate_disk_io)))
+
+rate_ram_io = results[:, 0, 0] / results[:, 0, 3] / 1024 / 1024
+print('   Rate, RAM I/O, {0}'.
+      format(rate_info_str(rate_ram_io)))
+
+rate_ram_i_null_o = results[:, 0, 0] / results[:, 0, 4] / 1024 / 1024
+print('   Rate, RAM I, null O, {0}'.
+      format(rate_info_str(rate_ram_i_null_o)))
